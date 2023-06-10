330f6f9ae249788d406146343cf5481e8753cc87
Gabriel Rosa <gabrieldsrosa@gmail.com>
2023-06-10 03:48:52 +0100

    feat: add tailwindcss and icons

---
+++ src/components/common/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <span>Capputeeno</span>
+    <input type="text" name="search" />
+    <div>Bag</div>
+  </header>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Header from '@/components/common/AppHeader.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Header />
   </main>
 </template>
