6302cb42f381489ee08fa21559313a9f61a1bded
Gabriel Rosa <gabrieldsrosa@gmail.com>
2023-06-10 05:34:08 +0100

    feat: add custom config for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: ['Saira', 'sans-serif'],
+      backgroundColor: {
+        icecream: '#DEDEDE'
+      }
+    }
   },
   plugins: []
 }
